02a6a7a47dc00c7c8c4f923c3b827168c692081d
thomastepi <tepexminem2013@gmail.com>
2024-02-23 16:37:16 -0500

    unknown route

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const app = express();
 
 app.use(cors({
@@ -9,13 +10,17 @@ app.use(cors({
 
 }));
 const port = process.env.PORT || 5000;
-
-
 require('./config/database');
 const userRoute = require('./routes/resumeUser.route');
 
 app.use(express.json());
 app.use('/api/user', userRoute);
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
