0b59b3cef8f205718343eeec8d0306d3966ea4b3
thomastepi <tepexminem2013@gmail.com>
2024-02-26 18:01:59 -0500

    update cors

--- server.js
+++ server.js
@@ -1,29 +1,34 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
 const app = express();
 
-const allowedOrigins = ['https://resume-craft.onrender.com', 'http://localhost:3000'];
+const allowedOrigins = [
+  "https://resume-craft.onrender.com",
+  "http://localhost:3000",
+  "https://www.thomastepi.com",
+];
 
-app.use(cors({
-  origin: allowedOrigins,
-  credentials: true
-
-}));
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  })
+);
 const port = process.env.PORT || 5000;
-require('./config/database');
-const userRoute = require('./routes/resumeUser.route');
-const portfolioRoute = require('./routes/portfolio.route');
+require("./config/database");
+const userRoute = require("./routes/resumeUser.route");
+const portfolioRoute = require("./routes/portfolio.route");
 
 app.use(express.json());
-app.use('/api/user', userRoute);
-app.use('/api/portfolio', portfolioRoute);
+app.use("/api/user", userRoute);
+app.use("/api/portfolio", portfolioRoute);
 
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "client/build")));
 
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 app.listen(port, () => {
